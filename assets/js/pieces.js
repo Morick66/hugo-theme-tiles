5cabae3adce87d09cf9f9e6f98e828adc0656ba3
ruixuan658 <ruixuana@gmail.com>
2024-08-09 00:04:49 +0800

    update

--- assets/js/pieces.js
+++ assets/js/pieces.js
@@ -193,10 +193,13 @@ document.addEventListener("DOMContentLoaded", function() {
         const commentDiv = document.createElement('div');
         commentDiv.id = commentDivId;
         twikooDiv.appendChild(commentDiv);
-
+        // 获取 id 为 'twikooEnvid' 的元素
+        var element = document.getElementById('twikooEnvid');
+        // 获取 data-evid 属性的值
+        var envid = element.getAttribute('data-envid');
         twikoo.init({
             el: `#${commentDivId}`,
-            envId: 'https://twikoo.morick66.com/',
+            envId: envid,
             path: url.href,
         });
     }
